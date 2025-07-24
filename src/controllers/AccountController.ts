fa432ab7e29f8b785ffcb4b80bcd6dc8a6f853c0
ericndungutse <dav.ndungutse@gmail.com>
2025-07-24 08:46:07 +0200

    feat(registration): add handler to routes that preserves this keyward in controller

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -11,15 +11,19 @@ class AccountController {
   }
 
   async createUser(req: Request, res: Response, next: NextFunction): Promise<void> {
-    const userDto: IAccountCreateResponseDto = req.body;
-    await this.accountService.createUser(userDto);
+    try {
+      const userDto: IAccountCreateResponseDto = req.body;
+      await this.accountService.createUser(userDto);
 
-    const response: ApiResponse<string, null> = {
-      status: 'success',
-      message: 'User created successfully',
-    };
+      const response: ApiResponse<string, null> = {
+        status: 'success',
+        message: 'User created successfully',
+      };
 
-    res.status(201).json(response);
+      res.status(201).json(response);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,8 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
+
 const express = require('express');
 const { container } = require('../infrastructure/container');
 
 const accountRouter = express.Router();
 
-accountRouter.post('/register', container.accountController.createUser);
+accountRouter.post('/register', (req: Request, res: Response, next: NextFunction) =>
+  container.accountController.createUser(req, res, next)
+);
 
 module.exports = accountRouter;
